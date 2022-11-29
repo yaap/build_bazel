650f29079b223851717b58c95b942ba5d65ea812
Cole Faust <colefaust@google.com>
2022-11-29 15:19:37 -0800

    Save collect.zip and difftool.zip paths before running soong again
    Now that standalone bazel builds and mixed builds both place
    their convenience symlinks in out/soong/workspace, if you run
    a b build followed by a mixed build you won't be able to access
    the b build artifacts via the convenience symlinks.
    
    Bug: 234449134
    Test: ./build/bazel/ci/diffs.sh
    Change-Id: I381d849bdf48a1527c6cfa1ee590c7ca0f6a1005

--- ci/diffs.sh
+++ ci/diffs.sh
@@ -9,8 +9,10 @@ build/soong/soong_ui.bash \
   --all-modules \
   --dir="$(pwd)" \
   bp2build
-build/bazel/bin/bazel build --config=bp2build //build/bazel/scripts/difftool:collect_zip
-build/bazel/bin/bazel build --config=bp2build //build/bazel/scripts/difftool:difftool_zip
+build/bazel/bin/bazel build --config=bp2build //build/bazel/scripts/difftool:collect_zip //build/bazel/scripts/difftool:difftool_zip
+
+readonly COLLECT_ZIP="$(realpath bazel-bin/build/bazel/scripts/difftool/collect.zip)"
+readonly DIFFTOOL_ZIP="$(realpath bazel-bin/build/bazel/scripts/difftool/difftool.zip)"
 
 # the following 2 arrays must be of the same size
 MODULES=(
@@ -57,7 +59,7 @@ for ((i = 0; i < ${#MODULES[@]}; i++)); do
   MODULE=${MODULES[$i]}
   echo "Building $MODULE for comparison"
   build/soong/soong_ui.bash --make-mode "$MODULE"
-  $stub_python "bazel-bin/build/bazel/scripts/difftool/collect.zip" \
+  $stub_python $COLLECT_ZIP \
     "$NINJA_FILE" "$LEGACY_COLLECTION"
   # TODO(b/254572169): Remove DISABLE_ARTIFACT_PATH_REQUIREMENT before launching --bazel-mode.
   build/soong/soong_ui.bash \
@@ -67,7 +69,7 @@ for ((i = 0; i < ${#MODULES[@]}; i++)); do
     BAZEL_STARTUP_ARGS="--max_idle_secs=5" \
     BAZEL_BUILD_ARGS="--color=no --curses=no --noshow_progress" \
     "$MODULE"
-  $stub_python "bazel-bin/build/bazel/scripts/difftool/collect.zip" \
+  $stub_python $COLLECT_ZIP \
       "$NINJA_FILE" "$MIXED_COLLECTION"
   OUTPUT=${OUTPUTS[$i]}
   for ((j = 0; j < ${#PATH_FILTERS[@]}; j++)); do
@@ -82,7 +84,7 @@ for ((i = 0; i < ${#MODULES[@]}; i++)); do
     mkdir -p "$MIXED_COLLECTION_DIR"
     cp "$MIXED_OUTPUT" "$MIXED_COLLECTION_DIR"
 
-    $stub_python "bazel-bin/build/bazel/scripts/difftool/difftool.zip" \
+    $stub_python $DIFFTOOL_ZIP \
       --level=SEVERE -v "$LEGACY_COLLECTION" "$MIXED_COLLECTION" \
       -l="$LEGACY_OUTPUT" -r="$MIXED_OUTPUT"
   done
