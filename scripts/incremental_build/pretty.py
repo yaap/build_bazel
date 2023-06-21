72b814c372f5f42e79a8e301f18ae8aaa43ab1c5
Usta Shrestha <usta@google.com>
2023-06-21 14:59:46 -0400

    clone.sh for manual testing
    this tool makes copies of a module and adds them to prod allowlist for mixed build
    
    Test: NA
    Bug: NA
    Change-Id: Ide93771203dbbe51419f330ab4541b5561370eb8

--- scripts/incremental_build/pretty.py
+++ scripts/incremental_build/pretty.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-import argparse
 # Copyright (C) 2022 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +11,7 @@ import argparse
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import argparse
 import csv
 import datetime
 import itertools
