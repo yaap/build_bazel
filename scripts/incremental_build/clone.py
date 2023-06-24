b47f59e8595ea17cb1ac551ed624b0285c3819a9
Usta Shrestha <usta@google.com>
2023-06-24 02:57:55 -0400

    clone multiple modules
    Note that much of Android.bp manipulation is being done with regexes in
    Python code; alternatively we could leverage bpmodify with some changes
    
    Test: ./incremental_build.sh -c "1x" -p prod
    Bug: 274668660
    Change-Id: I01ae604727d59c6fbb781a128805e87f7273f03f

--- scripts/incremental_build/clone.py
+++ scripts/incremental_build/clone.py
@@ -12,150 +12,362 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import argparse
-import dataclasses
 import functools
 import logging
+import os
 import re
 import shutil
 import uuid
 from pathlib import Path
-from typing import Final
+from string import Template
+from typing import Callable, Generator, Iterable
+from typing import NewType
+from typing import Optional
+from typing import TextIO
 
+import cuj
 import util
 from cuj import CujGroup
 from cuj import CujStep
 from cuj import src
+from go_allowlists import GoAllowlistManipulator
 
 _ALLOWLISTS = "build/soong/android/allowlists/allowlists.go"
 
+ModuleType = NewType("ModuleType", str)
+ModuleName = NewType("ModuleName", str)
 
-@dataclasses.dataclass
-class GoList:
-    lines: list[str]
-    begin: int = -1
-    end: int = -1
-
-    def has_module(self, module: str) -> bool:
-        for i in range(self.begin, self.end):
-            if f'"{module}"' in self.lines[i]:
-                return True
-        return False
-
-    def insert_clones(self, module: str, count: int):
-        clones = [f'\t\t"{module}-{i + 1}",\n' for i in range(0, count)]
-        self.lines = self.lines[0 : self.begin] + clones + self.lines[self.begin :]
-        self.end += count
-
-    def locate(self, listname: str):
-        start = re.compile(r"^\s*{l}\s=\s*\[]string\{{\s*$".format(l=listname))
-        self.begin = -1
-        for i, line in enumerate(self.lines):
-            if self.begin == -1 and start.match(line):
-                self.begin = i + 1
-            elif self.begin != -1 and line.strip() == "}":
-                self.end = i
-                return
-        raise RuntimeError(f"{listname} not found")
+Filter = Callable[[ModuleType, ModuleName], bool]
 
 
-def _allowlist(module: str, count: int):
-    """Add clones of `module` to bazel enabled allow lists"""
-    with open(src(_ALLOWLISTS), "r+") as file:
-        golist = GoList(file.readlines())
-        golist.locate("ProdMixedBuildsEnabledList")
-        golist.insert_clones(module, count)
-        golist.locate("Bp2buildModuleAlwaysConvertList")
-        if golist.has_module(module):
-            golist.insert_clones(module, count)
-
-        file.seek(0)
-        file.writelines(golist.lines)
-
-
-def _clone(androidbp: Path, module: str, count: int):
+def module_defs(src_lines: TextIO) -> Generator[tuple[ModuleType, str], None, None]:
     """
-    In the given Android.bp file, find the `module` and make `count` copies
-    which are identical except for their names that are '{module}-{i}'
-    for i=1 to `count`
+    Split `scr_lines` (an Android.bp file) into module definitions and discard
+    everything else (e.g. top level comments and assignments)
+    Assumes that the Android.bp file is properly formatted, specifically,
+    for any module definition:
+     1. the first line matches `start_pattern`, e.g. `cc_library {`
+     2. the last line matches a closing curly brace, i.e. '}'
     """
-    name_pattern = re.compile(
-        r'^(?P<prefix>\s*name:\s*"){mod}(?P<suffix>",?)$'.format(mod=module)
-    )
-    # we'll assume that the Android.bp file is properly formatted,
-    # specifically, for any module definition:
-    # 1. its first line matches `start_pattern` and
-    # 2. its last line matches a closing curly brace, i.e. '}'
-    start_pattern = re.compile(r"^(?P<module_type>\w+)\s*\{\s*$")  # e.g. `cc_library {`
-    nameline: int = -1
-    with open(androidbp, "r+") as f:
-        buffer: list[str] = []
-        for line in f:
-            if start_pattern.match(line):
-                buffer = [line]
-            elif line.rstrip() == "}":
-                if nameline != -1:
-                    buffer.append("}")
-                    break
-                else:
-                    buffer.clear()
-            else:
-                if nameline == -1:
-                    found = name_pattern.match(line) is not None
-                    if not found:
-                        buffer.clear()
-                    else:
-                        nameline = len(buffer)
-                if len(buffer):
-                    # buffer would be empty if a module definition has not started
-                    # or the module definition didn't name-match
-                    buffer.append(line)
+    start_pattern = re.compile(r"^(?P<module_type>\w+)\s*\{\s*$")
+    module_type: Optional[ModuleType] = None
+    buffer = ""
 
-        if nameline == -1:
-            raise RuntimeError(f"Couldn't find {module} in {androidbp}")
+    def in_module_def() -> bool:
+        return buffer != ""
 
-        f.seek(0, 2)  # go to the end of the file
-        for i in range(1, count + 1):
-            for j, line in enumerate(buffer):
-                if j == nameline:
-                    line = re.sub(
-                        name_pattern, f"\\g<prefix>{module}-{i}\\g<suffix>", line
-                    )
-                f.write(line)
-            f.write("\n")
-
-
-def clone_and_bazel_enable(androidbp: Path, module: str, count: int):
-    _allowlist(module, count)
-    _clone(androidbp, module, count)
-
-
-def get_cuj_group(androidbp: Path, module: str) -> CujGroup:
-    marker = uuid.uuid4()
-    androidbp_bak: Final[Path] = util.get_out_dir().joinpath(f"android.bp.{marker}")
-    allowlist_bak: Final[Path] = util.get_out_dir().joinpath(f"allowlists.go.{marker}")
-
-    def helper(count: int):
-        if not androidbp_bak.exists():
-            assert not allowlist_bak.exists()
-            # if first cuj_step then back up files to restore later
-            shutil.copy(androidbp, androidbp_bak)
-            shutil.copy(src(_ALLOWLISTS), allowlist_bak)
+    for line in src_lines:
+        # NB: line includes ending newline
+        line = line.replace("$", "$$")  # escape Templating meta-char '$'
+        if not in_module_def():
+            m = start_pattern.match(line)
+            if m:
+                module_type = ModuleType(m.group("module_type"))
+                buffer = line
         else:
-            shutil.copy(androidbp_bak, androidbp)
-            shutil.copy(allowlist_bak, src(_ALLOWLISTS))
-        clone_and_bazel_enable(androidbp, module, count)
+            buffer += line
+            if line.rstrip() == "}":
+                assert in_module_def()
+                # end of module definition
+                yield module_type, buffer
+                module_type = None
+                buffer = ""
 
-    def revert():
-        shutil.move(androidbp_bak, androidbp)
-        shutil.move(allowlist_bak, src(_ALLOWLISTS))
 
-    counts: Final[list[int]] = [1, 500, 1000, 5000, 10_000, 15_000]
-    steps = [
-        CujStep(verb=str(count), apply_change=functools.partial(helper, count))
-        for count in counts
-    ]
-    steps.append(CujStep(verb="0", apply_change=revert))
-    return CujGroup(f"clone-{module}", steps)
+def negate(fltr: Filter) -> Filter:
+    def f(t: ModuleType, n: ModuleName) -> bool:
+        return not fltr(t, n)
+
+    return f
+
+
+def conjunction(*fs: Filter) -> Filter:
+    def f(t: ModuleType, n: ModuleName) -> bool:
+        for fi in fs:
+            if not fi(t, n):
+                return False
+        return True
+
+    return f
+
+
+def type_in(*module_types: str) -> Filter:
+    def f(t: ModuleType, _: ModuleName) -> bool:
+        return t in module_types
+
+    return f
+
+
+def name_in(*module_names: str) -> Filter:
+    def f(_: ModuleType, n: ModuleName) -> bool:
+        return n in module_names
+
+    return f
+
+
+def _modify_genrule_template(module_name: ModuleName, module_def: str) -> Optional[str]:
+    # assume `out` only occurs as top-level property of a module
+    # assume `out` is always a singleton array
+    p = re.compile(r'[\n\r]\s+out\s*:\s*\[[\n\r]*\s*"[^"]+(?=")', re.MULTILINE)
+    g = p.search(module_def)
+    if g is None:
+        logging.debug('Could not find "out" for "%s"', module_name)
+        return None
+    index = g.end()
+    return f"{module_def[: index]}-${{suffix}}{module_def[index:]}"
+
+
+def _extract_templates_helper(src_lines: TextIO, f: Filter) -> dict[ModuleName, Template]:
+    """
+    For `src_lines` from an Android.bp file, find modules that satisfy the
+    Filter `f` and for each such mach return a "template" text that facilitates
+    changing the module's name.
+    """
+    # assume `name` only occurs as top-level property of a module
+    name_pattern = re.compile(r'[\n\r]\s+name:\s*"(?P<name>[^"]+)(?=")', re.MULTILINE)
+    result = dict[ModuleName, Template]()
+    for module_type, module_def in module_defs(src_lines):
+        m = name_pattern.search(module_def)
+        if not m:
+            continue
+        module_name = ModuleName(m.group("name"))
+        if module_name in result:
+            logging.debug(
+                f"{module_name} already exists thus " f"ignoring {module_type}"
+            )
+            continue
+        if not f(module_type, module_name):
+            continue
+        i = m.end()
+        module_def = f"{module_def[: i]}-${{suffix}}{module_def[i:]}"
+        if module_type == ModuleType("genrule"):
+            module_def = _modify_genrule_template(module_name, module_def)
+            if module_def is None:
+                continue
+        result[module_name] = Template(module_def)
+    return result
+
+
+def _extract_templates(
+    bps: dict[Path, Filter]
+) -> dict[Path, dict[ModuleName, Template]]:
+    """
+    If any key is a directory instead of an Android.bp file, expand it is as if it
+    were the glob pattern $key/**/Android.bp, i.e. replace it with all Android.bp
+    files under its tree.
+    """
+    bp2templates = dict[Path, dict[ModuleName, Template]]()
+    with open(src(_ALLOWLISTS), "rt") as af:
+        go_allowlists = GoAllowlistManipulator(af.readlines())
+        alwaysconvert = go_allowlists.locate("Bp2buildModuleAlwaysConvertList")
+
+    def maybe_register(bp: Path):
+        with open(bp, "rt") as src_lines:
+            templates = _extract_templates_helper(src_lines, fltr)
+            if not go_allowlists.is_dir_allowed(bp.parent):
+                templates = {n: v for n, v in templates.items() if n in alwaysconvert}
+            if len(templates) == 0:
+                logging.debug("No matches in %s", k)
+            else:
+                bp2templates[bp] = bp2templates.get(bp, {}) | templates
+
+    for k, fltr in bps.items():
+        if k.name == "Android.bp":
+            maybe_register(k)
+        for root, _, _ in os.walk(k):
+            if Path(root).is_relative_to(util.get_out_dir()):
+                continue
+            file = Path(root).joinpath("Android.bp")
+            if file.exists():
+                maybe_register(file)
+
+    return bp2templates
+
+
+@functools.cache
+def _back_up_path() -> Path:
+    #  a directory to back up files that these CUJs change
+    return util.get_out_dir().joinpath("clone-cuj-backup")
+
+
+def _backup(bps: Iterable[Path]):
+    # if first cuj_step then back up files to restore later
+    if _back_up_path().exists():
+        raise RuntimeError(
+            f"{_back_up_path()} already exists. "
+            f"Did you kill a previous cuj run? "
+            f"Delete {_back_up_path()} and revert changes to "
+            f"allowlists.go and Android.bp files"
+        )
+    for bp in bps:
+        src_path = bp.relative_to(util.get_top_dir())
+        bak_file = _back_up_path().joinpath(src_path)
+        os.makedirs(os.path.dirname(bak_file))
+        shutil.copy(bp, bak_file)
+    src_allowlists = src(_ALLOWLISTS)
+    bak_allowlists = _back_up_path().joinpath(_ALLOWLISTS)
+    os.makedirs(os.path.dirname(bak_allowlists))
+    shutil.copy(src_allowlists, bak_allowlists)
+
+
+def _restore():
+    src(_ALLOWLISTS).touch(exist_ok=True)
+    for root, _, files in os.walk(_back_up_path()):
+        for file in files:
+            bak_file = Path(root).joinpath(file)
+            bak_path = bak_file.relative_to(_back_up_path())
+            src_file = util.get_top_dir().joinpath(bak_path)
+            shutil.copy(bak_file, src_file)
+            # touch to update mtime; ctime is ignored by ninja
+            src_file.touch(exist_ok=True)
+
+
+def _bz_counterpart(bp: Path) -> Path:
+    return (
+        util.get_out_dir()
+        .joinpath("soong", "bp2build", bp.relative_to(util.get_top_dir()))
+        .with_name("BUILD.bazel")
+    )
+
+
+def _make_clones(bp2templates: dict[Path, dict[ModuleName, Template]], n: int):
+    r = f"{str(uuid.uuid4()):.6}"  # cache-busting
+    source_count = 0
+    output = ["\n"]
+
+    with open(src(_ALLOWLISTS), "rt") as f:
+        go_allowlists = GoAllowlistManipulator(f.readlines())
+        mixed_build_enabled_list = go_allowlists.locate("ProdMixedBuildsEnabledList")
+        alwaysconvert = go_allowlists.locate("Bp2buildModuleAlwaysConvertList")
+
+    def _allow():
+        if name not in mixed_build_enabled_list:
+            mixed_build_enabled_list.prepend([name])
+        mixed_build_enabled_list.prepend(clones)
+
+        # use the following to denylist instead of allowlisting
+        # go_allowlists.locate("Bp2buildModuleDoNotConvertList").prepend(clones)
+        if name in alwaysconvert:
+            alwaysconvert.prepend(clones)
+
+    for bp, n2t in bp2templates.items():
+        source_count += len(n2t)
+        output.append(
+            f"{n:5,}X{len(n2t):2,} modules = {n * len(n2t):+5,} "
+            f"in {bp.relative_to(util.get_top_dir())}"
+        )
+        with open(bp, "a") as f:
+            for name, t in n2t.items():
+                clones = []
+                for n in range(1, n + 1):
+                    suffix = f"{r}-{n:05d}"
+                    f.write(t.substitute(suffix=suffix))
+                    clones.append(ModuleName(f"{name}-{suffix}"))
+                _allow()
+
+    with open(src(_ALLOWLISTS), "wt") as f:
+        f.writelines(go_allowlists.lines)
+
+    logging.info(
+        f"Cloned {n:,}X{source_count:,} modules = {n * source_count:+,} "
+        f"in {len(bp2templates)} Android.bp files"
+    )
+    logging.debug("\n".join(output))
+
+
+def _display_sizes():
+    file_count = 0
+    orig_tot = 0
+    curr_tot = 0
+    output = ["\n"]
+    for root, _, files in os.walk(_back_up_path()):
+        file_count += len(files)
+        for file in files:
+            backup_file = Path(root).joinpath(file)
+            common_path = backup_file.relative_to(_back_up_path())
+            source_file = util.get_top_dir().joinpath(common_path)
+            curr_size = os.stat(source_file).st_size
+            curr_tot += curr_size
+            orig_size = os.stat(backup_file).st_size
+            orig_tot += orig_size
+            output.append(
+                f"{orig_size:7,} {curr_size - orig_size :+5,} => {curr_size:9,} "
+                f"bytes {source_file.relative_to(util.get_top_dir())}"
+            )
+            if file == "Android.bp":
+                bz = _bz_counterpart(source_file)
+                output.append(
+                    f"{os.stat(bz).st_size:8,} bytes "
+                    f"$OUTDIR/{bz.relative_to(util.get_out_dir())}"
+                )
+    logging.info(
+        f"Affected {file_count} files {orig_tot:,} "
+        f"{curr_tot - orig_tot:+,} => {curr_tot:,} bytes"
+    )
+    logging.debug("\n".join(output))
+
+
+def _name_cuj(count: int, module_count: int, bp_count: int) -> str:
+    match module_count:
+        case 1:
+            name = f"{count}"
+        case _:
+            name = f"{count}x{module_count}"
+    if bp_count > 1:
+        name = f"{name}({bp_count} files)"
+    return f"{name}x"
+
+
+def get_cuj_group(bps: dict[Path, Filter], group_name: str) -> CujGroup:
+    bp2templates = _extract_templates(bps)
+    bp_count = len(bp2templates)
+    if bp_count == 0:
+        raise RuntimeError(f"No eligible module to clone in {bps.keys()}")
+    module_count = sum(len(templates) for templates in bp2templates.values())
+
+    if "CLONE" in os.environ:
+        counts = [int(s) for s in os.environ["CLONE"].split(",")]
+    else:
+        counts = [1, 100, 200, 300, 400]
+        logging.info(
+            f'Will clone {",".join(str(i) for i in counts)} in cujs. '
+            f"You may specify alternative counts with CLONE env var, "
+            f"e.g. CLONE = 1,10,100,1000"
+        )
+    first_bp = next(iter(bp2templates.keys()))
+
+    def modify_bp():
+        with open(first_bp, mode="a") as f:
+            f.write(f"//post clone modification {uuid.uuid4()}\n")
+
+    steps: list[CujStep] = []
+    for i, count in enumerate(counts):
+        base_name = _name_cuj(count, module_count, bp_count)
+        steps.append(
+            CujStep(
+                verb=base_name,
+                apply_change=cuj.sequence(
+                    functools.partial(_backup, bp2templates.keys())
+                    if i == 0
+                    else _restore,
+                    functools.partial(_make_clones, bp2templates, count),
+                ),
+                verify=_display_sizes,
+            )
+        )
+        steps.append(CujStep(verb=f"bp aft {base_name}", apply_change=modify_bp))
+        if i == len(counts) - 1:
+            steps.append(
+                CujStep(
+                    verb="revert",
+                    apply_change=cuj.sequence(
+                        _restore, lambda: shutil.rmtree(_back_up_path())
+                    ),
+                    verify=_display_sizes,
+                )
+            )
+
+    return CujGroup(group_name, steps)
 
 
 def main():
@@ -173,7 +385,7 @@ def main():
     p.add_argument(
         "--count",
         "-n",
-        default=100,
+        default=1,
         type=int,
         help="number of times to clone; default: %(default)s",
     )
@@ -186,7 +398,9 @@ def main():
         help="absolute path to Android.bp file; default=%(default)s",
     )
     options = p.parse_args()
-    clone_and_bazel_enable(options.androidbp, options.module, options.count)
+    _make_clones(
+        _extract_templates({options.androidbp: name_in(options.module)}), options.count
+    )
     logging.warning("Changes made to your source tree; TIP: `repo status`")
 
 
