a30706e2ea3b762f7474070bdc44b799d5a7ddcc
usta <usta@google.com>
2023-07-22 21:19:09 -0400

    additional metrics during CUJs
    Test: SOONG_PROFILE_CPU=xyz incremental_build.sh -c "modify Android.bp"
    Bug: 274668660
    Change-Id: Ibe0adb835c58e54b913f34ac803ffdc8ebdfd2c6

--- scripts/incremental_build/perf_metrics.py
+++ scripts/incremental_build/perf_metrics.py
@@ -18,6 +18,8 @@ import datetime
 import glob
 import json
 import logging
+import os
+import pathlib
 import re
 import shutil
 import subprocess
@@ -56,18 +58,42 @@ class PerfInfoOrEvent:
 
 BP2BUILD_PB = "bp2build_metrics.pb"
 BUILD_TRACE_GZ = "build.trace.gz"
+CRITICAL_PATH = "soong.log"
 SOONG_BUILD_PB = "soong_build_metrics.pb"
 SOONG_PB = "soong_metrics"
+
+
+def _archive_pprof(envvar: str, d:Path):
+    if envvar not in os.environ:
+        return
+    pprof_prefix = pathlib.Path(os.environ[envvar])
+    if not pprof_prefix.is_absolute():
+        logging.warning(
+            "Ignoring pprof files; please use an absolute path, e.g. "
+            f"{envvar}={util.get_out_dir().joinpath('pprof')}"
+        )
+        return
+    for profile in os.listdir(str(pprof_prefix.parent)):
+        if profile.startswith(f"{pprof_prefix.name}."):
+            shutil.move(pprof_prefix.with_name(profile), d)
 
 
 def archive_run(d: Path, build_info: util.BuildInfo):
     with open(d.joinpath(util.BUILD_INFO_JSON), "w") as f:
         json.dump(build_info, f, indent=True, cls=util.CustomEncoder)
-    metrics_to_copy = [BP2BUILD_PB, BUILD_TRACE_GZ, SOONG_BUILD_PB, SOONG_PB]
+    metrics_to_copy = [
+        BP2BUILD_PB,
+        BUILD_TRACE_GZ,
+        CRITICAL_PATH,
+        SOONG_BUILD_PB,
+        SOONG_PB,
+    ]
     for metric_name in metrics_to_copy:
         metric = util.get_out_dir().joinpath(metric_name)
         if metric.exists():
             shutil.copy(metric, d.joinpath(metric_name))
+    _archive_pprof("SOONG_PROFILE_CPU", d)
+    _archive_pprof("SOONG_PROFILE_MEM", d)
 
 
 def read_pbs(d: Path) -> tuple[dict[str, any], list[PerfInfoOrEvent]]:
