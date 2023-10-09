27275e72ee2db2b30f67db8f3d665e8d6e44c22a
Jingwen Chen <jingwen@google.com>
2023-10-09 15:01:23 +0000

    Exclude all tradefed tests in bp2build.sh.
    The general Android tests that we care about are in
    roboleaf_|aunched.txt, and these are tested in atest_integration_test.
    
    There is no need to burden bp2build.sh with running tradefed tests.
    bp2build.sh should only be responsible for building targets, not testing
    them. Furthermore, bp2build-incremental is not set up to handle device
    tests, which already limits its usefulness.
    
    Test: presubmit
    Change-Id: Ie9a685d20c7f0fbfdfc9b29e15d83a5df325571a

--- ci/build_with_bazel.sh
+++ ci/build_with_bazel.sh
@@ -98,6 +98,6 @@ function build_and_test_for_host() {
   build/bazel/bin/bazel \
     "${STARTUP_FLAGS[@]}" test ${FLAGS[@]} \
     --build_tests_only=false \
-    --test_lang_filters=-tradefed_device_driven,-tradefed_host_driven_device \
+    --test_lang_filters=-tradefed_device_driven,-tradefed_host_driven_device,-tradefed_deviceless \
     -- ${targets[@]}
 }
